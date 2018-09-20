7f9fd66fdc4cfbe8a9b2d04218d5aa69ea219a75
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2018-09-20 01:05:57 -0400

    More comments

--- Voting/Simple/index.js
+++ Voting/Simple/index.js
@@ -1,9 +1,12 @@
+Web3 = require('web3');
+
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
 VotingContract = web3.eth.contract(abi);
 
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0xcab7600642d5b304498ecd41bc173b0fcb9b73f4');
+// node init.js
+contractInstance = VotingContract.at('0x9ce4f8e73976b98d1184d6b3364e8ccf88a483e2');
 
 candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
 
--- Voting/Simple/init.js
+++ Voting/Simple/init.js
@@ -1,14 +1,22 @@
+// Will use an in- memory blockchain
+// (think of it as a blockchain simulator) called ganache
+// ganache running on localhost:8545 using command:
+//      node_modules/.bin/ganache-cli
+
 Web3 = require('web3');
 solc = require('solc');
 fs = require("fs");
 
 // Initialize the solc and web3 objects
+
+// Connect to Ganache blockchain thorough web3
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 // To make sure web3 object is initialized and can communicate with the blockchain, let’s query all the accounts in the blockchain.
 console.log("The accounts in the blockchain are: \n", web3.eth.accounts);
 
-// To compile the contract, load the code from Voting.sol in to a string variable and compile it.
+// To compile the contract, load the code from Voting.sol
+// in to a string variable and compile it.
 code = fs.readFileSync('./Voting.sol').toString();
 compiledCode = solc.compile(code);
 // console.log("compiledCode is: ", compiledCode);
@@ -18,9 +26,10 @@ compiledCode = solc.compile(code);
     in the node console to see the contents), there are two important fields you will notice which
     are important to understand:
 
-    compiledCode.contracts[‘:Voting’].bytecode: This is the bytecode you get when the source code
+    1. compiledCode.contracts[‘:Voting’].bytecode: This is the bytecode you get when the source code
     in Voting.sol is compiled. This is the code which will be deployed to the blockchain.
-    compiledCode.contracts[‘:Voting’].interface: This is an interface or template of
+
+    2. compiledCode.contracts[‘:Voting’].interface: This is an interface or template of
     the contract (called abi) which tells the contract user what methods are available in the contract.
     Whenever you have to interact with the contract in the future, you will need this abi definition. You can read more details about ABI here
 */
@@ -31,16 +40,23 @@ VotingContract = web3.eth.contract(abiDefinition);
 
 byteCode = compiledCode.contracts[':Voting'].bytecode
 
-//  VotingContract.new above deploys the contract to the blockchain
-/*  The first argument is an array of candidates who are competing in the election */
+//  VotingContract.new below deploys the contract to the blockchain
+/*  First argument is an array of candidates who are competing in the election */
+//  Second argument:
+//    data - This is the compiled bytecode which we deploy to the blockchain
+//    from - Who deployed the contract on the blockchain to keep track of it
+//    data - This is the compiled bytecode which we deploy to the blockchain
+//    gas -  It costs money to interact with the blockchain. This money goes to
+//           miners who do all the work to include your code in the blockchain.
 deployedContract = VotingContract.new (
     ['Rama','Nick','Jose'],
-    {data: byteCode, from: web3.eth.accounts[0], gas: 4700000},
+    {data: byteCode, from: web3.eth.accounts[0], gas: 4700000}, // here gas is how much willing to pay
     (error, contract) => { // TODO: Weird that this callback fired twice
         if (error) {
           console.error("error is: ", error);
         }
         else if (contract.address === undefined) {
+            console.error("contract.address === undefined");
         }
         else {
             console.log("contract.address is: ", contract.address);
